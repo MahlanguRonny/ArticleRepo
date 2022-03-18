ff40e306c6d293bdf210af507ab85c70594956e4
aaronm <RonnyM@automationexchange.co.za>
2022-03-18 22:51:49 +0200

    ran first db migration(alll required EF packages installed)

---
+++ ArenaHoldings.ArticleManagement.Api/ArenaHoldings.ArticleManagement.Api/DataEntities/ArticleContext.cs
@@ -0,0 +1,14 @@
+﻿using ArenaHoldings.ArticleManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArenaHoldings.ArticleManagement.Api.DataEntities
+{
+    public class ArticleContext: DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+        { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Article> Articles { get; set; }
+    }
+}
--- ArenaHoldings.ArticleManagement.Api/ArenaHoldings.ArticleManagement.Api/Models/User.cs
+++ ArenaHoldings.ArticleManagement.Api/ArenaHoldings.ArticleManagement.Api/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using ArenaHoldings.ArticleManagement.Api.DataEntities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,13 @@ namespace ArenaHoldings.ArticleManagement.Api.Models
     public class User: IEntity
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
         public string Name { get; set; }
-        public string Email { get; set; } 
+        [Required]
+        [StringLength(40, ErrorMessage = "Maximum of 40 characters allowed")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        public ICollection<Article> Articles { get; set; }
     }
 }
